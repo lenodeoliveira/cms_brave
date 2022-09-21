9186b61f983ce6ca42a2e85f5c64bf6861cf8aa2
Lenodeoliveira <john.oliveira@brave.ag>
2022-09-21 11:08:11 -0300

    fix: fix .gitgnore

---
+++ src/data/usecases/db-authentication.ts
@@ -0,0 +1,29 @@
+import { Authentication } from '@/domain/usecases/authentication'
+import { LoadAccountByEmailRepository, UpdateAccessTokenRepository } from '@/data/protocols/db/account'
+import { HashComparer, Encrypter } from '@/data/protocols/cryptography/' 
+
+export class DbAuthentication implements Authentication {
+    constructor (
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
+    private readonly hashComparer: HashComparer,
+    private readonly encrypter: Encrypter,
+    private readonly updateAccessTokenRepository: UpdateAccessTokenRepository
+    ) {}
+
+    async auth (authenticationParams: Authentication.Params): Promise<Authentication.Result> {
+        const account = await this.loadAccountByEmailRepository.loadByEmail(authenticationParams.email)
+        if (account) {
+            const isValid = true
+            await this.hashComparer.compare(authenticationParams.password, account.password)
+            if (isValid) {
+                const accessToken = await this.encrypter.encrypt(account.name)
+                await this.updateAccessTokenRepository.updateAccessToken(account.id, accessToken)
+                return {
+                    accessToken,
+                    name: account.name
+                }
+            }
+        }
+        return null
+    }
+}

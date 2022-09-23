5ec5da0c6f13e2b54fd07f63627d9cbd504e9635
Lenodeoliveira <john.oliveira@brave.ag>
2022-09-23 15:26:30 -0300

    docs: add login API docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,29 @@
+import { loginPath } from './paths/login-path'
+import { accountSchema } from './schemas/account-schema'
+import { loginParamsSchema } from './schemas/login-params-schema'
+
 export default {
     openapi: '3.0.0',
     info: {
         title: 'CMS Brave',
         description: 'API desenvolvida para servir de base para projetos internos que precisem de um gerenciador de conteúdo',
         version: '1.0.0'
-    }
+    },
+    servers: [{
+        url: '/api'
+    }],
+    tags: [
+        {
+            name: 'Login',
+        }
+    ],
+    paths: {
+        '/login': loginPath
+    },
+    schemas: {
+        account: accountSchema,
+        loginParams: loginParamsSchema
+    },
+    
+    
 }

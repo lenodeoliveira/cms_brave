76d6f6942d622e8cb03d918a6fb381f52b8ae648
Lenodeoliveira <john.oliveira@brave.ag>
2022-09-23 14:56:39 -0300

    feat: add Swagger API

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,10 +1,12 @@
 import setupMiddlewares from '@/main/config/middlewares'
 import setupRoutes from '@/main/config/routes'
+import setupSwagger from '@/main/config/config-swagger'
 
 import express, { Express } from 'express'
 
 export const setupApp = async (): Promise<Express> => {
     const app = express()
+    setupSwagger(app)
     setupMiddlewares(app)
     setupRoutes(app)
     return app
---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+    openapi: '3.0.0',
+    info: {
+        title: 'CMS Brave',
+        description: 'API desenvolvida para servir de base para projetos internos que precisem de um gerenciador de conteúdo',
+        version: '1.0.0'
+    }
+}

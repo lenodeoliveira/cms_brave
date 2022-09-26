218eece887a48f3a2452e2ccbb4aeb4239d0e789
Lenodeoliveira <john.oliveira@brave.ag>
2022-09-26 13:32:18 -0300

    test: add tests in route login

--- tests/main/routes/login-routes.test.ts
+++ tests/main/routes/login-routes.test.ts
@@ -10,15 +10,48 @@ describe('Login Routes', () => {
         app = await setupApp()
     })
 
-    test('Should return an account on success', async () => {
-        await request(app)
+    test('Should return an bad request if name is not passed', async () => {
+        const res = await request(app)
             .post('/api/signup')
             .send({
-                name: 'any_email',
                 email: 'any@gmail.com',
                 password: '12345678',
                 passwordConfirmation: '12345678'
             })
-            .expect(500)
+        expect(res.statusCode).toBe(400)
+        expect(res.body).toEqual({ error: 'Misssing param: name' })
+    })
+
+    test('Should return an bad request if email is not passed', async () => {
+        const res = await request(app)
+            .post('/api/signup')
+            .send({
+                name: 'name',
+                password: '12345678',
+                passwordConfirmation: '12345678'
+            })
+        expect(res.body).toEqual({ error: 'Misssing param: email' })
+    })
+
+    test('Should return an bad request if password is not passed', async () => {
+        const res = await request(app)
+            .post('/api/signup')
+            .send({
+                name: 'name',
+                email: 'any@gmail.com',
+                passwordConfirmation: '12345678'
+            })
+        expect(res.body).toEqual({ error: 'Misssing param: password' })
+    })
+
+    test('Should return an bad request if password confirmation is not passed', async () => {
+        const res = await request(app)
+            .post('/api/signup')
+            .send({
+                name: 'name',
+                email: 'any@gmail.com',
+                password: '12345678',
+            })
+        expect(res.body).toEqual({ error: 'Misssing param: passwordConfirmation' })
     })
 })

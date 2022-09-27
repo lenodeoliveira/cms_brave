46421f51caf87858bb416e959ff59b035c655b34
Lenodeoliveira <john.oliveira@brave.ag>
2022-09-27 16:19:11 -0300

    feat: add route login

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,8 +1,10 @@
 import { adaptRoute } from '@/main/adapters/express-route-adapter'
 import { makeSignUpController } from '@/main/factories/controllers/signUp/signup-controller-factory'
+import { makeLoginController } from '@/main/factories/controllers/login/login-controller-factory'
 
 import { Router } from 'express'
 
 export default (router: Router): void => {
     router.post('/signup', adaptRoute(makeSignUpController()))
+    router.post('/login', adaptRoute(makeLoginController()))
 }

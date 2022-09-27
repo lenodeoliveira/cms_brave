34aca43ea851843998864b253942bad32a337aca
Lenodeoliveira <john.oliveira@brave.ag>
2022-09-27 18:28:19 -0300

    refactor: add types in connection class

--- src/infra/db/mysqldb/helpers/connection.ts
+++ src/infra/db/mysqldb/helpers/connection.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize'
 export class MyqslConnection {
-    static connection: Sequelize
-    static async connect(dbDatabase, dbUserName, dbPassword, dbHost): Promise<void> {
+    private static connection: Sequelize
+    static async connect(dbDatabase: string, dbUserName: string, dbPassword: string, dbHost: string): Promise<void> {
         const sequelize = new Sequelize(dbDatabase, dbUserName, dbPassword, {
             host: dbHost,
             dialect: 'mysql'

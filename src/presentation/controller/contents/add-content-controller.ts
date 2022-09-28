8bdbb54bf1bc49da70722e9b60c233ced4017dd7
Lenodeoliveira <john.oliveira@brave.ag>
2022-09-28 11:00:42 -0300

    feat: ensure AddContentController returns a badRequest error if Validation fails

--- src/presentation/controller/contents/add-content-controller.ts
+++ src/presentation/controller/contents/add-content-controller.ts
@@ -1,3 +1,4 @@
+import { badRequest } from '@/presentation/helpers/http/http-helpers'
 import { Controller } from '@/presentation/protocols/controller'
 import { HttpResponse } from '@/presentation/protocols/http'
 import { Validation } from '@/presentation/protocols/validation'
@@ -7,7 +8,10 @@ export class AddContentController implements Controller {
     constructor (private readonly validation: Validation) {}
 
     async handle(request: AddContentController.Result): Promise<HttpResponse> {
-        this.validation.validate(request)
+        const error = this.validation.validate(request)
+        if(error) {
+            return badRequest(error)
+        }
         return Promise.resolve(null)
     }
 }

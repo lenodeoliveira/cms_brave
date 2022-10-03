4a550e61e7dd8aa431f45bb67b45fb305cafe55f
Lenodeoliveira <john.oliveira@brave.ag>
2022-10-03 08:48:45 -0300

    feat: add route to serve static files

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,11 +1,13 @@
 import setupMiddlewares from '@/main/config/middlewares'
 import setupRoutes from '@/main/config/routes'
+import setupStaticFiles from '@/main/config/static-files'
 import setupSwagger from '@/main/config/config-swagger'
 
 import express, { Express } from 'express'
 
 export const setupApp = async (): Promise<Express> => {
     const app = express()
+    setupStaticFiles(app)
     setupSwagger(app)
     setupMiddlewares(app)
     setupRoutes(app)

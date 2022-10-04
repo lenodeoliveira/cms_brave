14aa4a557854a524ce65dc4cf444d2f96b9dad8d
Lenodeoliveira <john.oliveira@brave.ag>
2022-10-04 18:06:10 -0300

    test: make ensure FileValidation returns an error if the imagem type is wrong

--- tests/validation/validators/file-validation.spec.ts
+++ tests/validation/validators/file-validation.spec.ts
@@ -14,4 +14,12 @@ describe('CompareFieldsValidation', () => {
         })
         expect(error).toEqual(new Error('Invalid size'))
     })
+    test('Should return an error if image type validation fails', () => {
+        const sut = makeSut()
+        const error = sut.validate({
+            size: 121234,
+            mimetype: 'image/gif'
+        })
+        expect(error).toEqual(new Error('Invalid type'))
+    })
 })

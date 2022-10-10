8b9a09bbb59b5104e678f814df639d2c2d1e96cb
Lenodeoliveira <john.oliveira@brave.ag>
2022-10-10 15:15:37 -0300

    test: ensures it returns 204 if the content is deleted

--- tests/presentation/controller/contents/remove-content-controller.spec.ts
+++ tests/presentation/controller/contents/remove-content-controller.spec.ts
@@ -1,7 +1,7 @@
 import { RemoveContentSpy } from '../../mocks/mock-content'
 import { RemoveContentController } from '@/presentation/controller/contents/remove-content-controller'
 import { throwError } from '@/../tests/domain/test-helpers'
-import { serverError } from '@/presentation/helpers/http/http-helpers'
+import { noContent, ok, serverError } from '@/presentation/helpers/http/http-helpers'
 
 type SutTypes = {
   sut: RemoveContentController
@@ -27,6 +27,12 @@ describe('RemoveContent Controller', () => {
         const request = makeFakeRequest()
         await sut.handle(request)
         expect(removeContentSpy.id).toEqual(request.id)
+    })
+
+    test('Should return 200 on success', async () => {
+        const { sut } = makeSut()
+        const httpRequest = await sut.handle(makeFakeRequest())
+        expect(httpRequest).toEqual(noContent())
     })
 
     test('Should return 500 if RemoveContent throws', async () => {

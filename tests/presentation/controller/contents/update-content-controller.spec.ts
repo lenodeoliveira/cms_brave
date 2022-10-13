aabe79aa675e0320ec62cae6eba84a8867398cbc
Lenodeoliveira <john.oliveira@brave.ag>
2022-10-13 14:50:09 -0300

    test: ensures that the correct value is passed to UpdateContent

---
+++ tests/presentation/controller/contents/update-content-controller.spec.ts
@@ -0,0 +1,32 @@
+import { UpdateContentSpy } from '../../mocks/mock-content'
+import { UpdateContentController } from '@/presentation/controller/contents/update-content-controller'
+import { throwError } from '@/../tests/domain/test-helpers'
+import { noContent, notFound, serverError } from '@/presentation/helpers/http/http-helpers'
+
+type SutTypes = {
+  sut: UpdateContentController
+  updateContentSpy: UpdateContentSpy
+}
+
+const makeFakeRequest = (): UpdateContentController.Result => ({
+    id: 'any_id'
+})
+
+const makeSut = (): SutTypes => {
+    const updateContentSpy = new UpdateContentSpy()
+    const sut = new UpdateContentController(updateContentSpy)
+    return {
+        sut,
+        updateContentSpy
+    }
+}
+
+
+describe('UpdateContent Controller', () => {
+    test('Should call UpdateContent with correct values', async () => {
+        const { sut, updateContentSpy } = makeSut()
+        const request = makeFakeRequest()
+        await sut.handle(request)
+        expect(updateContentSpy.id).toEqual(request.id)
+    })
+})

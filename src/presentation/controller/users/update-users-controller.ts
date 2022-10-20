7d6af78e6248539a292912a34277d96fe7e11f9e
Lenodeoliveira <john.oliveira@brave.ag>
2022-10-20 16:34:28 -0300

    feat: ensures that an exception is thrown if UpdateUserByAdmin throws a

--- src/presentation/controller/users/update-users-controller.ts
+++ src/presentation/controller/users/update-users-controller.ts
@@ -2,7 +2,7 @@ import { Controller } from '@/presentation/protocols/controller'
 import { HttpResponse } from '@/presentation/protocols/http'
 import { UpdateUserByAdmin } from '@/domain/usecases/users/update-user'
 import { Validation } from '../../protocols/validation'
-import { badRequest } from '@/presentation/helpers/http/http-helpers'
+import { badRequest, serverError } from '@/presentation/helpers/http/http-helpers'
 
 export class UpdateUserByAdminController implements Controller {
     constructor (
@@ -11,12 +11,17 @@ export class UpdateUserByAdminController implements Controller {
     ) {}
 
     async handle (request: UpdateUserByAdminController.Request): Promise<HttpResponse> {
-        const error = await this.validation.validate(request)
-        if(error) {
-            return badRequest(error)
+        try {
+            const error = await this.validation.validate(request)
+            if(error) {
+                return badRequest(error)
+            }
+            await this.updateUserByAdmin.registerUser(request)
+        } catch (error) {
+            return serverError(error)
         }
-        await this.updateUserByAdmin.registerUser(request)
-        return Promise.resolve(null)
+        
+        
     }
 }
 

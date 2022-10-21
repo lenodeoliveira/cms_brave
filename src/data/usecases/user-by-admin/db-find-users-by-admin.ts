af3a64c838c86123942d1d1788c33d17c3d2d91d
Lenodeoliveira <john.oliveira@brave.ag>
2022-10-21 13:38:31 -0300

    feat: ensure correct values are passed to DbFindUsersByAdmin

---
+++ src/data/usecases/user-by-admin/db-find-users-by-admin.ts
@@ -0,0 +1,13 @@
+import { FindUsersByAdminRepository } from '@/data/protocols/db/users-by-admin/find-users-by-admin-repository'
+import { FindUserByAdmin } from '@/domain/usecases/users/users-by-admin'
+
+export class DbFindUsersByAdmin implements FindUserByAdmin {
+    constructor (
+      private readonly findUsersByAdminRepository: FindUsersByAdminRepository
+    ){}
+    async findUsers (params: FindUserByAdmin.Params): Promise<FindUserByAdmin.Result> {
+        await this.findUsersByAdminRepository.findUsers(params)
+        return Promise.resolve(null)
+    }
+  
+}
--- tests/data/mocks/mock-db-user.ts
+++ tests/data/mocks/mock-db-user.ts
@@ -1,5 +1,7 @@
 import { RegisterUserByAdminRepository } from '@/data/protocols/db/users-by-admin/register-users-by-admin-repository'
 import { UpdateUserByAdminRepository } from '@/data/protocols/db/users-by-admin/update-users-by-admin-repository'
+import { mockUser } from '../../domain/mock-users'
+import { FindUsersByAdminRepository } from '../protocols/db/users-by-admin/find-users-by-admin-repository'
 
 export class RegisterUserByAdminRepositorySpy implements RegisterUserByAdminRepository {
     params: RegisterUserByAdminRepository.Params 
@@ -21,3 +23,14 @@ export class UpdateUserByAdminRepositorySpy implements UpdateUserByAdminReposito
         return this.result
     }
 }
+
+export class FindUserByAdminRepositorySpy implements FindUsersByAdminRepository {
+    params: FindUsersByAdminRepository.Params 
+    result = mockUser()
+
+    async findUsers (data: FindUsersByAdminRepository.Params): Promise<FindUsersByAdminRepository.Result> {
+        this.params = data
+        return this.result
+    }
+}
+

f42dc3a96897289ba11d7dfe9cf036d8913b080e
Lenodeoliveira <john.oliveira@brave.ag>
2022-10-21 10:52:47 -0300

    test: ensures that FindUserByAdmin retrieves the correct values

---
+++ tests/presentation/controller/users/users-by-admin-controller.spec.ts
@@ -0,0 +1,35 @@
+import { UsersByAdminController } from '@/presentation/controller/users/user-by-admin-controller'
+import { FindUserByAdminSpy } from '../../mocks/mock-users'
+import { noContent, ok, serverError } from '@/presentation/helpers/http/http-helpers'
+import MockDate from 'mockdate'
+
+
+type SutTypes = {
+  sut: UsersByAdminController
+  findUserByAdminSpy: FindUserByAdminSpy
+}
+
+const makeSut = (): SutTypes => {
+    const findUserByAdminSpy = new FindUserByAdminSpy()
+    const sut = new UsersByAdminController(findUserByAdminSpy)
+    return {
+        sut,
+        findUserByAdminSpy
+    }
+}
+
+describe('UserbyAdminController', () => {
+    beforeAll(() => {
+        MockDate.set(new Date())
+    })
+
+    afterAll(() => {
+        MockDate.reset()
+    })
+
+    test('Should return 200 on success', async () => {
+        const { sut, findUserByAdminSpy } = makeSut()
+        const httpResponse = await sut.handle({})
+        expect(httpResponse).toEqual(ok(findUserByAdminSpy.result))
+    })
+})

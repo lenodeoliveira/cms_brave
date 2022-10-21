b65e29fa7c397fb6d7d5ac7cc06cabf811acd442
Lenodeoliveira <john.oliveira@brave.ag>
2022-10-21 14:21:21 -0300

    test: ensures that user recovery

--- tests/data/usecases/user-by-admin/db-find-user-by-admin.spec.ts
+++ tests/data/usecases/user-by-admin/db-find-user-by-admin.spec.ts
@@ -1,5 +1,6 @@
 import { DbFindUsersByAdmin } from '@/data/usecases/user-by-admin/db-find-users-by-admin'
 import { FindUserByAdminRepositorySpy } from '../../mocks'
+import { throwError } from '@/../tests/domain/test-helpers'
 
 type SutTypes = {
   sut: DbFindUsersByAdmin
@@ -20,5 +21,18 @@ describe('DbFindUsersByAdmin Usecase', () => {
         const { sut, findUserByAdminRepositorySpy } = makeSut()
         await sut.findUsers({ page: 2, limit: 2 })
         expect(findUserByAdminRepositorySpy.params).toEqual({ page: 2, limit: 2 })
+    })
+
+    test('Should throw if DbFindUsersByAdmin throws', async () => {
+        const { sut, findUserByAdminRepositorySpy } = makeSut()
+        jest.spyOn(findUserByAdminRepositorySpy, 'findUsers').mockImplementationOnce(throwError)
+        const promise = sut.findUsers({ page: 2, limit: 2 })
+        expect(promise).rejects.toThrow()  
+    })
+
+    test('Should retrieve users', async () => {
+        const { sut, findUserByAdminRepositorySpy } = makeSut()
+        const users = await sut.findUsers({})
+        expect(users).toEqual(findUserByAdminRepositorySpy.result)
     })
 })

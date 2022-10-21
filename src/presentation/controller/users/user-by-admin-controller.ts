22a74dcd61ae9088dd25d65c57f79a25cae4fb5b
Lenodeoliveira <john.oliveira@brave.ag>
2022-10-21 11:11:32 -0300

    feat: ensure throw an exception with status code 500 in case FindUserByAdmin throws an exception

--- src/presentation/controller/users/user-by-admin-controller.ts
+++ src/presentation/controller/users/user-by-admin-controller.ts
@@ -1,7 +1,8 @@
 import { FindUserByAdmin } from '@/domain/usecases/users/users-by-admin'
-import { noContent, ok } from '@/presentation/helpers/http/http-helpers'
+import { noContent, ok, serverError } from '@/presentation/helpers/http/http-helpers'
 import { Controller } from '@/presentation/protocols/controller'
 import { HttpResponse } from '@/presentation/protocols/http'
+
 
 export class UsersByAdminController implements Controller {
     constructor (
@@ -9,8 +10,12 @@ export class UsersByAdminController implements Controller {
     ) {}
 
     async handle (request: UsersByAdminController.Request): Promise<HttpResponse> {
-        const users = await this.findUserByAdmin.findUsers(request)
-        return users ? ok(users) : noContent()
+        try {
+            const users = await this.findUserByAdmin.findUsers(request)
+            return users ? ok(users) : noContent()
+        } catch (error) {
+            return serverError(error)
+        }
     }
 }
 

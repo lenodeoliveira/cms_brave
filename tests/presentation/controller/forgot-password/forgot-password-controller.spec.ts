7a7e4714c3ced9eb95438690afa608dc4aa232d9
Lenodeoliveira <john.oliveira@brave.ag>
2022-10-25 13:11:31 -0300

    test: ensures Validation is called with correct values

--- tests/presentation/controller/forgot-password/forgot-password-controller.spec.ts
+++ tests/presentation/controller/forgot-password/forgot-password-controller.spec.ts
@@ -1,18 +1,22 @@
 import {ForgotPasswordController} from '@/presentation/controller/forgot-password/forgot-password-controller'
 import {ForgotPasswordSpy} from '../../mocks/mock-forgot-password'
+import {ValidationSpy} from '../../mocks/mock-validation'
 
 type SutTypes = {
   sut: ForgotPasswordController
   forgotPasswordSpy: ForgotPasswordSpy
+  validationSpy: ValidationSpy
 }
 
 const makeSut = (): SutTypes => {
     const forgotPasswordSpy = new ForgotPasswordSpy()
-    const sut = new ForgotPasswordController(forgotPasswordSpy)
+    const validationSpy = new ValidationSpy()
+    const sut = new ForgotPasswordController(forgotPasswordSpy, validationSpy)
   
     return {
         sut,
-        forgotPasswordSpy
+        forgotPasswordSpy,
+        validationSpy
     }
 }
 
@@ -21,5 +25,11 @@ describe('ForgotPassWordController', () => {
         const { sut, forgotPasswordSpy } = makeSut()
         await sut.handle({email: 'any_mail@mail.com'})
         expect(forgotPasswordSpy.email).toEqual('any_mail@mail.com')
+    })
+
+    test('Should call validation with correct values', async () => {
+        const { sut, validationSpy } = makeSut()
+        await sut.handle({email: 'any_mail@mail.com'})
+        expect(validationSpy.input).toEqual({email: 'any_mail@mail.com'})
     })
 })

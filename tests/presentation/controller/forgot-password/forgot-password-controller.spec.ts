149f9d86eb39d20f5c6621439a47bfa5e0525056
Lenodeoliveira <john.oliveira@brave.ag>
2022-10-25 13:21:31 -0300

    test: ensures that the refund status code 404 if the user does not exist

--- tests/presentation/controller/forgot-password/forgot-password-controller.spec.ts
+++ tests/presentation/controller/forgot-password/forgot-password-controller.spec.ts
@@ -1,6 +1,8 @@
 import {ForgotPasswordController} from '@/presentation/controller/forgot-password/forgot-password-controller'
 import {ForgotPasswordSpy} from '../../mocks/mock-forgot-password'
 import {ValidationSpy} from '../../mocks/mock-validation'
+import {badRequest, forbidden, notFound} from '@/presentation/helpers/http/http-helpers'
+import {EmailInUseError} from '@/presentation/errors'
 
 type SutTypes = {
   sut: ForgotPasswordController
@@ -31,5 +33,19 @@ describe('ForgotPassWordController', () => {
         const { sut, validationSpy } = makeSut()
         await sut.handle({email: 'any_mail@mail.com'})
         expect(validationSpy.input).toEqual({email: 'any_mail@mail.com'})
+    })
+
+    test('Should return 400 if Validation fails', async () => {
+        const { sut, validationSpy } = makeSut()
+        validationSpy.error = new Error()
+        const httpResponse = await sut.handle({email: 'any_mail@mail.com'})
+        expect(httpResponse).toEqual(badRequest(validationSpy.error))
+    })
+
+    test('Should return 404 if user not exists', async () => {
+        const { sut, forgotPasswordSpy } = makeSut()
+        forgotPasswordSpy.result = false
+        const httpResponse = await sut.handle({email: 'any_mail@mail.com'})
+        expect(httpResponse).toEqual(notFound(new Error('User does not exist')))
     })
 })

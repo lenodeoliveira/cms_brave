bb23fa75eca5680bb296a25f5cb2850b465aa1ea
Lenodeoliveira <john.oliveira@brave.ag>
2022-10-25 13:11:07 -0300

    feat: ensures Validation is called with correct values

--- src/presentation/controller/forgot-password/forgot-password-controller.ts
+++ src/presentation/controller/forgot-password/forgot-password-controller.ts
@@ -1,11 +1,16 @@
 import {Controller} from '@/presentation/protocols/controller'
 import {HttpResponse} from '@/presentation/protocols/http'
 import {ForgotPassword} from '@/domain/usecases/forgot-password/forgot-password'
+import {Validation} from '@/presentation/protocols/validation'
 
 export class ForgotPasswordController implements Controller {
-    constructor(private  readonly forgoPassword: ForgotPassword) {}
+    constructor(
+      private  readonly forgoPassword: ForgotPassword,
+      private readonly validation: Validation,
+    ) {}
   
     async handle (request: ForgotPasswordController.Request): Promise<HttpResponse> {
+        await  this.validation.validate(request)
         await this.forgoPassword.generateToken(request.email)
         return Promise.resolve(null)
     }

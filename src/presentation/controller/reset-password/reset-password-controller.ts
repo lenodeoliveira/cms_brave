98024f22e6ef823968c13545c2a61bbe4f9f2e3f
Lenodeoliveira <john.oliveira@brave.ag>
2022-10-26 17:20:34 -0300

    feat: ensures it throws an error if Validation finds a

--- src/presentation/controller/reset-password/reset-password-controller.ts
+++ src/presentation/controller/reset-password/reset-password-controller.ts
@@ -2,6 +2,7 @@ import { Controller } from '@/presentation/protocols/controller'
 import { HttpResponse } from '@/presentation/protocols/http'
 import { Validation } from '@/presentation/protocols/validation'
 import { ResetUserPassword } from '@/domain/usecases/reset-password/reset-password'
+import { badRequest } from '@/presentation/helpers/http/http-helpers'
 
 export class ResetPasswordController implements Controller {
     constructor(
@@ -10,6 +11,13 @@ export class ResetPasswordController implements Controller {
     ){}
 
     async handle (request: ResetPasswordController.Request): Promise<HttpResponse> {
+        
+        const error = this.validation.validate(request)
+
+        if(error) {
+            return badRequest(error)
+        }
+      
         const { email, code, password } = request
         await this.resetPassword.resetPassword({ email, code, password})
         return Promise.resolve(null)

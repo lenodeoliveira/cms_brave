1a7f3035416f7e90eed47e7535840c0f787111cd
Lenodeoliveira <john.oliveira@brave.ag>
2022-10-26 17:09:51 -0300

    test: ensures that the correct value for ResetPassword is passed

---
+++ tests/presentation/controller/reset-password/reset-password-controller.spec.ts
@@ -0,0 +1,36 @@
+import { ResetUserPasswordSpy } from '../../mocks/mock-reset-password'
+import { ResetPasswordController } from '@/presentation/controller/reset-password/reset-password-controller'
+import { ValidationSpy } from '../../mocks/mock-validation'
+
+type SutType = {
+  sut: ResetPasswordController
+  validationSpy: ValidationSpy
+  resetUserPasswordSpy: ResetUserPasswordSpy
+}
+
+
+const makeSut = (): SutType => {
+    const resetUserPasswordSpy = new ResetUserPasswordSpy()
+    const validationSpy = new ValidationSpy()
+    const sut = new ResetPasswordController(resetUserPasswordSpy, validationSpy)
+
+    return {
+        sut, 
+        resetUserPasswordSpy,
+        validationSpy
+    }
+}
+
+const mockRequest = (): ResetPasswordController.Request => ({
+    password: 'any_password',
+    passwordConfirmation: 'any_password'
+}) 
+
+describe('ResetPasswordController', () => {
+    test('Should call ResetUserPassword the correct values', async () => {
+        const { sut, resetUserPasswordSpy } = makeSut()
+        const resetPasswordSpy = jest.spyOn(resetUserPasswordSpy, 'resetPassword')
+        await sut.handle(mockRequest())
+        expect(resetPasswordSpy).toHaveBeenCalledWith('any_password')
+    })
+})

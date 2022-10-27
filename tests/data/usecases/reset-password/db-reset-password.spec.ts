63eaf32dd2d672b5725679f1ba31a56b06e36442
Lenodeoliveira <john.oliveira@brave.ag>
2022-10-27 14:58:18 -0300

    test: ensure it throws an error if ResetPasswordRepository throws one

--- tests/data/usecases/reset-password/db-reset-password.spec.ts
+++ tests/data/usecases/reset-password/db-reset-password.spec.ts
@@ -1,25 +1,41 @@
 import { DbResetPassword } from '@/data/usecases/reset-password/db-reset-password'
+import { LoadAccountByEmailRepositorySpy } from '../../mocks'
 import { ResetPasswordRepositorySpy, TokenValidateRepositorySpy } from '../../mocks/mock-db-reset-password'
+import MockDate from 'mockdate'
+import { mockLoadAccountByEmail } from '@/../tests/domain/mock-account'
+import { throwError } from '@/../tests/domain/test-helpers'
 
 type SutType = {
   sut: DbResetPassword
   resetPasswordRepositorySpy: ResetPasswordRepositorySpy
-  tokenValidateRepositorySpy: TokenValidateRepositorySpy
+  tokenValidateRepositorySpy: TokenValidateRepositorySpy,
+  loadAccountByEmailRepositorySpy: LoadAccountByEmailRepositorySpy
 }
 
 const makeSut = (): SutType => {
     const resetPasswordRepositorySpy = new ResetPasswordRepositorySpy()
     const tokenValidateRepositorySpy = new TokenValidateRepositorySpy()
-    const sut = new DbResetPassword(resetPasswordRepositorySpy, tokenValidateRepositorySpy)
+    const loadAccountByEmailRepositorySpy = new LoadAccountByEmailRepositorySpy()
+    const sut = new DbResetPassword(resetPasswordRepositorySpy, tokenValidateRepositorySpy, loadAccountByEmailRepositorySpy)
 
     return {
         sut,
         resetPasswordRepositorySpy,
         tokenValidateRepositorySpy,
+        loadAccountByEmailRepositorySpy
     }
 }
 
 describe('DbResetPassword Usecase', () => {
+    beforeAll(() => {
+        MockDate.set(new Date())
+    })
+
+    afterAll(() => {
+        MockDate.reset()
+    })
+
+
     test('Should call ResetPasswordRepositorySpy with correct values', async () => {
         const { sut, resetPasswordRepositorySpy } = makeSut()
 
@@ -33,5 +49,16 @@ describe('DbResetPassword Usecase', () => {
             code: 'any_code',
             password: 'any_password',
         })
+    })
+
+    test('Should throw if ResetPasswordRepository throws', async () => {
+        const { sut, resetPasswordRepositorySpy } = makeSut()
+        jest.spyOn(resetPasswordRepositorySpy, 'resetPassword').mockImplementationOnce(throwError)
+        const promise = sut.resetPassword({
+            email: 'any_mail@mail.com',
+            code: 'any_code',
+            password: 'any_password',
+        })
+        await expect(promise).rejects.toThrow()
     })
 })

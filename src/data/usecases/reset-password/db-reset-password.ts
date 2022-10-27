2be0dbaef5dc0d1b80d121f9b055b2b4f00c676b
Lenodeoliveira <john.oliveira@brave.ag>
2022-10-27 15:10:37 -0300

    feat: ensures that an error is thrown if Hasher throws a

--- src/data/usecases/reset-password/db-reset-password.ts
+++ src/data/usecases/reset-password/db-reset-password.ts
@@ -2,19 +2,24 @@ import { ResetPasswordRepository } from '@/data/protocols/db/reset-password/rese
 import { TokenValidateRepository } from '@/data/protocols/db/token-validate/token-validate'
 import { LoadAccountByEmailRepository } from '@/data/protocols/db/account/load-account-by-email-repository'
 import { ResetUserPassword } from '@/domain/usecases/reset-password/reset-password'
+import { Hasher } from '@/data/protocols/cryptography'
 
 export class DbResetPassword implements ResetUserPassword {
     constructor(
       private readonly resetPasswordRepository: ResetPasswordRepository,
       private readonly tokenValidateRepository: TokenValidateRepository,
-      private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
+      private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
+      private readonly hasher: Hasher,
     ){}
 
     async resetPassword (params: ResetUserPassword.Params): Promise<string | boolean> {
         const { passwordResetToken, passwordResetExpires } = await this.loadAccountByEmailRepository.loadByEmail(params.email)
         const error = await this.tokenValidateRepository.validateToken({ passwordResetToken, passwordResetExpires })
         if (error) return error
-        return await this.resetPasswordRepository.resetPassword(params)
+
+        const hashedPassword = await this.hasher.hash(params.password)
+        
+        return await this.resetPasswordRepository.resetPassword({ ...params, password: hashedPassword })
     }
   
 } 
--- tests/data/usecases/reset-password/db-reset-password.spec.ts
+++ tests/data/usecases/reset-password/db-reset-password.spec.ts
@@ -1,5 +1,5 @@
 import { DbResetPassword } from '@/data/usecases/reset-password/db-reset-password'
-import { LoadAccountByEmailRepositorySpy } from '../../mocks'
+import { HasherSpy, LoadAccountByEmailRepositorySpy } from '../../mocks'
 import { ResetPasswordRepositorySpy, TokenValidateRepositorySpy } from '../../mocks/mock-db-reset-password'
 import MockDate from 'mockdate'
 import { mockLoadAccountByEmail } from '@/../tests/domain/mock-account'
@@ -9,20 +9,24 @@ type SutType = {
   sut: DbResetPassword
   resetPasswordRepositorySpy: ResetPasswordRepositorySpy
   tokenValidateRepositorySpy: TokenValidateRepositorySpy,
-  loadAccountByEmailRepositorySpy: LoadAccountByEmailRepositorySpy
+  loadAccountByEmailRepositorySpy: LoadAccountByEmailRepositorySpy,
+  hasherSpy: HasherSpy
 }
 
 const makeSut = (): SutType => {
     const resetPasswordRepositorySpy = new ResetPasswordRepositorySpy()
     const tokenValidateRepositorySpy = new TokenValidateRepositorySpy()
     const loadAccountByEmailRepositorySpy = new LoadAccountByEmailRepositorySpy()
-    const sut = new DbResetPassword(resetPasswordRepositorySpy, tokenValidateRepositorySpy, loadAccountByEmailRepositorySpy)
+    const  hasherSpy = new HasherSpy()
+    const sut = new DbResetPassword(resetPasswordRepositorySpy, tokenValidateRepositorySpy, loadAccountByEmailRepositorySpy, hasherSpy)
 
     return {
         sut,
         resetPasswordRepositorySpy,
         tokenValidateRepositorySpy,
-        loadAccountByEmailRepositorySpy
+        loadAccountByEmailRepositorySpy,
+        hasherSpy
+
     }
 }
 
@@ -47,13 +51,24 @@ describe('DbResetPassword Usecase', () => {
         expect(resetPasswordRepositorySpy.data).toEqual({
             email: 'any_mail@mail.com',
             code: 'any_code',
-            password: 'any_password',
+            password: 'any',
         })
     })
 
     test('Should throw if ResetPasswordRepository throws', async () => {
         const { sut, resetPasswordRepositorySpy } = makeSut()
         jest.spyOn(resetPasswordRepositorySpy, 'resetPassword').mockImplementationOnce(throwError)
+        const promise = sut.resetPassword({
+            email: 'any_mail@mail.com',
+            code: 'any_code',
+            password: 'any_password',
+        })
+        await expect(promise).rejects.toThrow()
+    })
+
+    test('Should throw if Hasher throws', async () => {
+        const { sut, hasherSpy } = makeSut()
+        jest.spyOn(hasherSpy, 'hash').mockImplementationOnce(throwError)
         const promise = sut.resetPassword({
             email: 'any_mail@mail.com',
             code: 'any_code',

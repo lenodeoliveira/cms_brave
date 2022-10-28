63b61ab7f6c0330df5e743be0e3a11c51e146c65
Lenodeoliveira <john.oliveira@brave.ag>
2022-10-28 18:01:17 -0300

    test: make sure it returns 204 if there is no content

--- tests/presentation/controller/contents/load-content-by-admin-controller.spec.ts
+++ tests/presentation/controller/contents/load-content-by-admin-controller.spec.ts
@@ -1,4 +1,5 @@
 import { LoadContentByAdminController } from '@/presentation/controller/contents/load-content-by-admin-controller'
+import { noContent } from '@/presentation/helpers/http/http-helpers'
 import { LoadContentByAdminSpy } from '../../mocks/mock-content'
 
 type SutTypes = {
@@ -21,5 +22,12 @@ describe('LoadContentByAdmin Controller', () => {
         const { sut, loadContentByAdminSpy } = makeSut()
         await sut.handle({id: 'any_id'})
         expect(loadContentByAdminSpy.id).toBe('any_id')
+    })
+
+    test('Should return status code 204 if no content exists', async () => {
+        const { sut, loadContentByAdminSpy } = makeSut()
+        loadContentByAdminSpy.result = null
+        const httpResponse = await sut.handle({id: 'any_id'})
+        expect(httpResponse).toEqual(noContent())
     })
 })
